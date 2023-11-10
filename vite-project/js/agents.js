02afda88c91a64576185db0509bd591c651a837e
Shenixx <113193025+Shenixx@users.noreply.github.com>
2023-11-10 10:14:18 -0500

    asdsadasdasd

---
+++ vite-project/js/agents.js
@@ -0,0 +1,12 @@
+const agents = [
+    {
+        name: "Raze",
+        role: "Duelist",
+        description: "Raze explodes out of Brazil with her big blunt-force-trauma playstyle, she excels at flushing entrenched enemies and clearing tight spaces with a generous dose of boom.",
+    },
+    {
+        name: ""
+    }
+]
+
+export {agents};
---
+++ vite-project/js/main.js
@@ -0,0 +1,12 @@
+import '../css/style.css';
+import './agents';
+import './dom'
+import {agents} from "./agents";
+
+console.log(agents);
+
+
+
+
+// const letMePass = cards.filter((card)=> card.rarity === "Common").forEach((common) => console.log(common));
+// const dontFail = cards.filter((card)=> card.printings.includes("5ED")).forEach((printing) => console.log(printing));
